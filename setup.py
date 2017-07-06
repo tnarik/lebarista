69235ebd111192e72d02eebf0c0a5536bfc57ba2
tnarik <tnarik@gmail.com>
2017-07-06 09:31:15 +0100

    Initial structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="LeBarista",
+    version="0.1.0",
+
+    # Application author details:
+    author="Tnarik Innael",
+    author_email="tnarik@lecafeautomatique.co.uk",
+
+
+    url="https://github.com/tnarik/lebarista",
+
+    # Modules
+    py_modules=["lebarista"],
+
+    # Packages
+    #packages=["lebarista"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    #
+    # license="LICENSE.txt",
+    description="A notifications tool.",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "slackclient",
+    ],
+)
